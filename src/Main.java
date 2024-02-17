8d7ae7bae8c979043dec4ac9e9bd1b8223fe8ee6
mac <artem.workamail.87@gmail.com>
2024-02-17 19:35:46 +0300

    начальная связь

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for ( int i = 2020; i < 2070; i = i + 4) {
+            System.out.println( " высокосный год " + i);
+        }
+        System.out.println( "Задача №1");
+
+
+    };
+
+}
